8e0f42983cbf0319df173bf25924b9acc329210c
Lavinia Masini <masini.lavinia@gmail.com>
2017-11-03 15:23:05 +0100

    added project

---
+++ myproject/src/main/java/org/mycompany/myproject/App.java
@@ -0,0 +1,15 @@
+package org.mycompany.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	public String sayHello() {
+		
+		return "Hello";
+		
+	}
+	
+}
---
+++ myproject/src/test/java/org/mycompany/myproject/AppTest.java
@@ -0,0 +1,25 @@
+package org.mycompany.myproject;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	private App app;
+	
+	@Before
+	public void init() {
+		app = new App();
+	}
+	
+	@Test
+	public void testSayHello() {
+		assertEquals("Hello", app.sayHello());
+	}
+	
+}
